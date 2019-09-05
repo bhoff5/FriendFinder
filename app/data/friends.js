c732e117cb37220daee94aa90f67b96090698662
Brian Hoff <blhoff52002@gmail.com>
2019-09-05 15:42:40 -0500

    updating js, comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,3 @@
 var friendsArray = [];
 
-// Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,34 @@
 var friendsData = require("../data/friends");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
   app.post("/api/friends", function(req, res) {
-    console.log(friendsData);
+    var newArr = [];
+    var matchIndex = 0;
+    var defaultDifference = 100;
+    req.body.scores.forEach(element => {
+      newArr.push(parseInt(element));
+    });
+    var userPoints = newArr.reduce((a, b) => a + b, 0);
+    req.body.scores = userPoints;
+
+    for (var i = 0; i < friendsData.length; i++) {
+      var difference = Math.abs(userPoints - friendsData[i].scores);
+      if (difference < defaultDifference) {
+        defaultDifference = difference;
+        matchIndex = i;
+      }
+      console.log(matchIndex);
+    }
+
     friendsData.push(req.body);
-    res.json(true);
+    res.json(friendsData[matchIndex]);
   });
 
   app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
     friendsData.length = 0;
     res.json({ ok: true });
   });
